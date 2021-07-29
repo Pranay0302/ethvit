4cafc8d24f8118ab39a187eef0a9745ad3bdd314
Pranay Kumar Andra <pka.pranayandra02@gmail.com>
2021-07-29 15:22:53 +0530

    initial commit
    setting up web3

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+var ethjs = require("web3")
+var rv = require('./price')
+
+var url = `https://mainnet.infura.io/v3/${process.env.projid}`
+
+var web3 = new ethjs(url)
+
+function x(val) {
+    return web3.utils.fromWei(val)
+}
+
+class Val {
+
+    gb() {
+        web3.eth.getBalance(process.env.addr, (err, bal) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('acnt balance:', x(bal), 'eth')
+            }
+        })
+    }
+
+}
+
+var val = new Val();
+val.gb()
+rv.returnVal()
---
+++ price.js
@@ -0,0 +1,11 @@
+const { default: axios } = require("axios");
+
+function returnVal() {
+    axios.get(process.env.aurl).then(res => {
+        console.log(`curr price: ${res.data[1].current_price}`)
+    })
+
+}
+
+
+module.exports = { returnVal }
